698c02ef242fb48a2a8f54b4a8f61fbc2be33784
Paullo <you@example.com>
2021-03-13 01:12:09 -0300

    Acomodando o ambiente
    criando pastas, organizando coisas pra organizar

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private TileBackground[,] allTiles;
+    
+
+
+
+    void Start()
+    {
+        allTiles = new TileBackground[width, height];
+        SetUp();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tmpPstn = new Vector2(i, j);
+                Instantiate(tilePrefab, tmpPstn, Quaternion.identity);
+            }
+        }
+    }
+}
