e176e918d2fada36bb95ee1416776a7d3607dbd1
Paullo <you@example.com>
2021-03-13 02:12:13 -0300

    Grid e Gemas Spawnando

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,31 +6,41 @@ public class Board : MonoBehaviour
 {
     public int width;
     public int height;
+    [Space(20)]
     public GameObject tilePrefab;
-    private TileBackground[,] allTiles;
+    [Space(20)]
+    public GameObject[] gems;
     
+    private TileBackground[,] allTiles;
+    public GameObject[,] allGems;
+
 
 
 
     void Start()
     {
-        allTiles = new TileBackground[width, height];
+        //allTiles = new TileBackground[width, height];
+        allGems = new GameObject[width, height];
         SetUp();
     }
 
-    void Update()
-    {
-        
-    }
 
-    private void SetUp()
+    void SetUp()
     {
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
+                //Acomodando os espaços
                 Vector2 tmpPstn = new Vector2(i, j);
-                Instantiate(tilePrefab, tmpPstn, Quaternion.identity);
+                GameObject bgTile = Instantiate(tilePrefab, tmpPstn, Quaternion.identity, this.gameObject.transform);
+                bgTile.name = "(" + i + "," + j + ")";
+                //Instanciando as jóias
+                int gemsAvailable = Random.Range(0, gems.Length);
+                GameObject gem = Instantiate(gems[gemsAvailable], tmpPstn, Quaternion.identity, this.transform);
+                gem.name = "(" + i + "," + j + ")";
+                allGems[i, j] = gem;
+
             }
         }
     }
